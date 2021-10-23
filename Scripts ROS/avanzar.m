c0099813964cfb2aa9d3de61771201e996797a99
Patricia C <patriciacuestaruiz@gmail.com>
2021-10-23 13:38:02 +0200

    todos los scripts para robot y ROS

---
+++ Scripts ROS/avanzar.m
@@ -0,0 +1,28 @@
+distancia = 1;
+
+msg_vel.Linear.X = 0.1;
+msg_vel.Linear.Y = 0;
+msg_vel.Linear.Z = 0;
+msg_vel.Angular.X = 0;
+msg_vel.Angular.Y = 0;
+msg_vel.Angular.Z = 0;
+
+% Guardamos la posición inicial
+initpos = sub_odom.LatestMessage.Pose.Pose.Position;
+
+while (1)
+   % Actualizamos la posición
+   pos = sub_odom.LatestMessage.Pose.Pose.Position;
+   d = sqrt((initpos.X - pos.X)^2 + (initpos.Y - pos.Y)^2); 
+   disp(d);
+   if (d > distancia)
+       msg_vel.Linear.X = 0;
+       send(pub_vel, msg_vel);
+       break;
+   else
+       send(pub_vel, msg_vel);
+   end
+   lee_sensores;
+   waitfor(r);
+end
+
---
+++ Scripts Robot/conectar.m
@@ -0,0 +1,3 @@
+clc; clear all; close all;
+
+rosinit('http://172.29.30.173:11311', 'NodeHost', '172.29.29.93');
